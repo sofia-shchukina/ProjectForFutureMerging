32944c8641f33a074639558713f391ba727da1e3
sonia <sofiashchukina@outlook.com>
2022-07-13 15:49:32 +0200

    Initial commmit

---
+++ src/main/java/com/example/projectforfuturemerging/ProjectForFutureMergingApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectforfuturemerging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectForFutureMergingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectForFutureMergingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectforfuturemerging/ProjectForFutureMergingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectforfuturemerging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectForFutureMergingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
